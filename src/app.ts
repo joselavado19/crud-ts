48b36670c140fc2051a7b1f7c23414ca7672e018
joselavado19 <j_antonio76@hotmail.com>
2024-01-12 22:23:03 -0500

    n

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
-import express,  {type Application, Request, Response} from "express";
+import express, { type Application } from "express";
 import cors from "cors";
+import { userRouter, productRouter } from "./components";
 
 const app: Application = express();
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req: Request, res: Response): Response => {
-    return res.json({
-        message :"HOla mundo",
-    });
-});
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 
 export default app;
--- src/network/responses.ts
+++ src/network/responses.ts
@@ -1,18 +1,27 @@
 import type { Response } from "express";
 
 interface IResponse {
-  status: number;
+  status?: number;
   res: Response;
   data: any;
 }
 
-export function responseSuccess({ res, data, status }: IResponse): Response {
+export function responseSuccess({
+  res,
+  data,
+  status = 200,
+}: IResponse): Response {
   return res.status(status).json({
     ok: true,
     data,
   });
 }
-export function responseError({ res, data, status }: IResponse): Response {
+
+export function responseError({
+  res,
+  data,
+  status = 500,
+}: IResponse): Response {
   return res.status(status).json({
     ok: false,
     data,
